3b3baafdb6cff428357b23fb1b8c89ecf53fb2dc
Mark Thomas <markthethomas@gmail.com>
2015-01-07 17:03:56 -0800

    Created passing solution

--- function_spies/program.js
+++ function_spies/program.js
@@ -1,5 +1,17 @@
 function Spy(target, method) {
-  // SOLUTION GOES HERE
+  // Set up original function
+  var originalFunction = target[method]
+  // A way to keep track of results. We can keep better track of count
+  var result = {
+    count: 0
+  }
+  // we alias the target & method to a funcion that then calls the original function, but with the important difference that we actually call the original function later by using apply
+  target[method] = function () {
+    result.count ++
+    return originalFunction.apply(this, arguments)
+  }
+  // Eventually return the result
+  return result
 }
 
 module.exports = Spy
