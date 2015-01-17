f578f081553f4cbbd50207931ed88bef86a79c7e
Mark Thomas <markthethomas@gmail.com>
2015-01-16 17:13:47 -0800

    Added summary Added a summary for myself; I find it helpful to go through a concept I don't understand by writing out the general idea in plain english.

--- trampoline/program.js
+++ trampoline/program.js
@@ -1,19 +1,22 @@
+// Summary
+// The repeat function takes an operation and will repeat it num times. The trampoline function takes a function as its primary argument and runs a while loop while fn is a function; the while loop serves as the primary means of being a wrapper for the repeat function, since it will be iterative-ish and allow us to re-execute the function over and over again without stacking up frames. 
+
 function repeat(operation, num) {
   return function() {
     if (num <= 0) return
-      operation()
-      return repeat(operation, --num)
-    }
+    operation()
+    return repeat(operation, --num)
   }
+}
 
-  function trampoline(fn) {
-    while(typeof fn === 'function') {
-      fn = fn()
-    }
+function trampoline(fn) {
+  while (typeof fn === 'function') {
+    fn = fn()
   }
+}
 
-  module.exports = function(operation, num) {
-    trampoline(function() {
-      return repeat(operation, num)
-    })
-  }
+module.exports = function(operation, num) {
+  trampoline(function() {
+    return repeat(operation, num)
+  })
+}
