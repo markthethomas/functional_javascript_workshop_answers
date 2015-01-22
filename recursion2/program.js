397a3e7380b9ee17d7bf3f2358c61507e3310929
Mark Thomas <markthethomas@gmail.com>
2015-01-22 11:07:41 -0800

    Added step by step explanation

--- recursion2/program.js
+++ recursion2/program.js
@@ -1,15 +1,24 @@
 'use strict';
 
 function getDependencies(mod, result) {
+  // Initialize result and dependencies to either the passed argument or an array
+  // result keeps track of the found and sorted deps
   result = result || [];
   var dependencies = mod.dependencies || [];
+  // Get the keys of dependencies and run forEach
   Object.keys(dependencies).forEach(function(dep) {
+    // var key takes the passed element (as dep) and gets the version from the dep
     var key = dep + '@' + mod.dependencies[dep].version;
-    if (result.indexOf(key) === -1) result.push(key)
-      getDependencies(mod.dependencies[dep], result);
+    // if the result is not in result already, push the key into the array
+    if (result.indexOf(key) === -1) {
+      result.push(key)
+    }
+    // recurse and pass the function the specific dep and a result
+    getDependencies(mod.dependencies[dep], result);
   });
+  // sort results alphabetically
   return result.sort();
 }
 
 
-module.exports = getDependencies
+module.exports = getDependencies;
